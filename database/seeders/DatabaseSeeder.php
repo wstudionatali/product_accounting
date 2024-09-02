01b658dae457f21d2a039bee08b29f91880b2ec0
wstudionatali <wstudionatali@gmail.com>
2024-09-02 22:41:42 +0200

    initial data seeding for the first admin and all user roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,43 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the application's database
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+    /**
+     * Seed tables user_roles and users with 1-st admin data
+     */
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+    $admin_id = DB::table('user_roles')->insertGetId(
+        [
+        'role' =>  env('ADMINISTRATOR', 'admin'),
+        ],
+        );
+    DB::table('user_roles')->insert([
+        [
+        'role' =>  env('STOREKEEPER', 'storekeeper'),
+            ],
+        [
+        'role' =>  env('SALES_MANAGER', 'sales_manager'),
+        ],
+        [
+        'role' =>  env('SALESMAN', 'salesman'),
+        ],
+    ]);
+
+    User::factory()->create([
+        'name' => 'Natalia',
+        'email' => 'wstudionatali@gmail.com',
+        'password' => Hash::make('admin'),
+        'user_role_id' => $admin_id,
+    ]);
+
     }
 }
